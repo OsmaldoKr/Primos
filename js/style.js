cd4acd6abc78386b0b4233991afc6652426afefb
OsmaldoKr <Osmaldo2006@gmail.com>
2023-07-11 22:23:46 -0600

    act

--- js/style.js
+++ js/style.js
@@ -1,10 +1,11 @@
-const $btnSignIn= document.querySelector('.sign-in-btn'),
-       $btnSignUp = document.querySelector('.sign-up-btn'),
-       $signUp = document.querySelector('.sign-up'),
-       $signIn = document. querySelector('.sign-in');
-document. addEventListener ('click', e => {
-     if (e. target === $btnSignIn || e. target === $btnSignUp){
-         $signIn. classList. toggle('active');
-         $signUp. classList. toggle('active')
-    }
+const $btnSignIn = document.querySelector('.sign-in-btn');
+const $btnSignUp = document.querySelector('.sign-up-btn');
+const $signUp = document.querySelector('.sign-up');
+const $signIn = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+  if (e.target === $btnSignIn || e.target === $btnSignUp) {
+    $signIn.classList.toggle('active');
+    $signUp.classList.toggle('active');
+  }
 });
