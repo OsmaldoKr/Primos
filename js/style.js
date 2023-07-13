c9516cc5e67ba1be0e74948b433cd565f730a216
OsmaldoKr <Osmaldo2006@gmail.com>
2023-07-12 18:50:51 -0600

    act

--- js/style.js
+++ js/style.js
@@ -12,3 +12,13 @@ document.addEventListener('click', e => {
 function redirectToLogin(){
   window.location.href = '/html/Principal.html'
 }
+
+function redirectSocialIng(){
+  window.location.href =  'https://instagram.com/osmaldokr?igshid=NGExMmI2YTkyZg==' 
+}
+function redirectSocialFb(){
+  window.location.href =  'https://www.facebook.com/profile.php?id=100093133827196' 
+}
+function redirectSocialTT(){
+  window.location.href =  'https://www.tiktok.com/@osmaldo.16?_t=8dwQZMNUDt0&_r=1' 
+}
